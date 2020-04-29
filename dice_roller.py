5f8a5a84c5546b82cf68457a7d60165508dfbb72
jessicasyan <64444720+jessicasyan@users.noreply.github.com>
2020-04-28 21:35:14 -0700

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -5,8 +5,10 @@ def main():
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
     dice_sum += roll
-    print(f'You rolled a {roll}')
-print(f'You have rolled a total of {dice_sum}')
+    if roll == 1:
+      print(f'You rolled a {roll}! Critical Fail')
+    else: print(f'You rolled a {roll}')
+  print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
